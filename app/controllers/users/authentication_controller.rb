96313990033da5bf2d275f92dbd131f8e1160730
yannick lescure <yannick.lescure@gmail.com>
2021-04-09 09:33:43 -0400

    improve code + fix bugs

--- app/controllers/users/authentication_controller.rb
+++ app/controllers/users/authentication_controller.rb
@@ -33,54 +33,54 @@ module Users
       # binding.pry
       # resource.sessionIpAddress = request.remote_ip
       render json: MultiJson.dump({
-        facebookAuth: @facebookAuth,
+        # facebookAuth: @facebookAuth,
         id: resource.id,
         email: resource.email,
-        slug: resource.slug,
+        # slug: resource.slug,
         first_name: resource.first_name,
         last_name: resource.last_name,
-        name: resource.name,
-        admin: resource.admin,
-        authentication_token: resource.authentication_token,
-        image: resource.image,
-        checked: resource.checked,
-        mailchimp: resource.mailchimp,
-        notification: resource.notification,
-        locale: resource.locale,
-        moderator: resource.moderator,
-        freemium: resource.freemium,
-        likes: Like.where(user_id: resource.id),
-        commentLikes: CommentLike.where(user_id: resource.id).map { |r| r.comment_id },
-        replyLikes: ReplyLike.where(user_id: resource.id).map { |r| r.reply_id },
-        bookmarks: Bookmark.where(user_id: resource.id),
-        followers: {
-          count: resource.followers.length,
-          data: resource.followers.map { |f| {
-              name: f.name,
-              slug: f.slug,
-              checked: f.checked,
-              image: {
-                thumb: {
-                  url: f.image.url(:thumb)
-                }
-              }
-            }
-          }
-        },
-        following: {
-          count: resource.following.length,
-          data: resource.following.map { |f| {
-              name: f.name,
-              slug: f.slug,
-              checked: f.checked,
-              image: {
-                thumb: {
-                  url: f.image.url(:thumb)
-                }
-              }
-            }
-          }
-        }
+        # name: resource.name,
+        # admin: resource.admin,
+        # authentication_token: resource.authentication_token,
+        # image: resource.image,
+        # checked: resource.checked,
+        # mailchimp: resource.mailchimp,
+        # notification: resource.notification,
+        # locale: resource.locale,
+        # moderator: resource.moderator,
+        # freemium: resource.freemium,
+        # likes: Like.where(user_id: resource.id),
+        # commentLikes: CommentLike.where(user_id: resource.id).map { |r| r.comment_id },
+        # replyLikes: ReplyLike.where(user_id: resource.id).map { |r| r.reply_id },
+        # bookmarks: Bookmark.where(user_id: resource.id),
+        # followers: {
+        #   count: resource.followers.length,
+        #   data: resource.followers.map { |f| {
+        #       name: f.name,
+        #       slug: f.slug,
+        #       checked: f.checked,
+        #       image: {
+        #         thumb: {
+        #           url: f.image.url(:thumb)
+        #         }
+        #       }
+        #     }
+        #   }
+        # },
+        # following: {
+        #   count: resource.following.length,
+        #   data: resource.following.map { |f| {
+        #       name: f.name,
+        #       slug: f.slug,
+        #       checked: f.checked,
+        #       image: {
+        #         thumb: {
+        #           url: f.image.url(:thumb)
+        #         }
+        #       }
+        #     }
+        #   }
+        # }
       })
     end
   end
