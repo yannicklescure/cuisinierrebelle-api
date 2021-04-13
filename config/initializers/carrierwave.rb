319218d28640c27dae09c559217142e76a7204c6
yannick lescure <yannick.lescure@gmail.com>
2021-04-13 09:25:35 -0400

    improve code + fix bugs

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,13 +6,13 @@ CarrierWave.configure do |config|
 
   # Use AWS storage if in production
   config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV['S3_KEY'],                # required unless using use_iam_profile
-    aws_secret_access_key: ENV['S3_SECRET'],             # required unless using use_iam_profile
-    use_iam_profile:       false,                         # optional, defaults to false
-    region:                ENV['S3_REGION'],             # optional, defaults to 'us-east-1'
-    host:                  ENV['S3_ASSET_URL'],            # optional, defaults to nil
-    endpoint:              ENV['S3_CDN_URL'] # optional, defaults to nil
+    provider:              'AWS',               # required
+    aws_access_key_id:     ENV['S3_KEY'],       # required unless using use_iam_profile
+    aws_secret_access_key: ENV['S3_SECRET'],    # required unless using use_iam_profile
+    use_iam_profile:       false,               # optional, defaults to false
+    region:                ENV['S3_REGION'],    # optional, defaults to 'us-east-1'
+    host:                  ENV['S3_CDN_URL'], # optional, defaults to nil
+    endpoint:              ENV['S3_ASSET_URL']    # optional, defaults to nil
   }
   config.fog_directory  = ENV['S3_BUCKET_NAME']                                      # required
   config.fog_public     = false                                                 # optional, defaults to true
