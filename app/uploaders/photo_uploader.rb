5a7c0c485ed4bb297ce0064138087f5319da026a
yannick lescure <yannick.lescure@gmail.com>
2021-04-13 09:00:18 -0400

    improve code + fix bugs

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -10,12 +10,11 @@ class PhotoUploader < CarrierWave::Uploader::Base
   include WebPConverter
 
   # Choose what kind of storage to use for this uploader:
-  # if Rails.env.development? || Rails.env.test?
-  #   storage :file
-  # elsif Rails.env.production?
-  #   # storage :fog
-  #   storage :aws
-  # end
+  if Rails.env.development? || Rails.env.test?
+    storage :file
+  elsif Rails.env.production?
+    storage :fog
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
