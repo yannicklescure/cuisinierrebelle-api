e483467b07915dcb99a8ba4806326238a94ab7fc
yannick lescure <yannick.lescure@gmail.com>
2021-04-21 23:55:58 -0400

    improve code + fix bugs

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -90,7 +90,7 @@ class Api::V1::UsersController < Api::V1::BaseController
                     url: f.image.url(:thumb)
                   }
                 },
-                createdAt: (user.follower_relationships.find_by(following_id: user.id, follower_id: f.id).created_at.to_f * 1000).to_i
+                createdAt: (@user.follower_relationships.find_by(following_id: @user.id, follower_id: f.id).created_at.to_f * 1000).to_i
               }
             }.compact.sort_by { |hsh| hsh[:createdAt] }
           },
@@ -105,7 +105,7 @@ class Api::V1::UsersController < Api::V1::BaseController
                     url: f.image.url(:thumb)
                   }
                 },
-                createdAt: (user.following_relationships.find_by(following_id: f.id, follower_id: user.id).created_at.to_f * 1000).to_i
+                createdAt: (@user.following_relationships.find_by(following_id: f.id, follower_id: @user.id).created_at.to_f * 1000).to_i
               }
             }.compact.sort_by { |hsh| hsh[:createdAt] }
           },
