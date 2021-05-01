fb1c4cfff05c47ffa430d0c608c9c48ddab5ddc8
yannick lescure <yannick.lescure@gmail.com>
2021-04-30 21:43:55 -0400

    improve code + fix bugs

--- app/controllers/api/v1/notification_controller.rb
+++ app/controllers/api/v1/notification_controller.rb
@@ -2,20 +2,14 @@ class Api::V1::NotificationController < Api::V1::BaseController
   # acts_as_token_authentication_handler_for User, except: [ :index, :show ]
   before_action :authenticate_and_set_user
   # before_action :authenticate_user!
-  before_action :set_user, only: [ :update ]
 
   def update
     # binding.pry
-    @user.notification = params[:notification] == 'true'
-    @user.save
-    render json: { notification: @user.notification }
-  end
-
-  private
-
-  def set_user
     # @user = User.find(params[:id])
     @user = current_user
     authorize @user  # For Pundit
+    @user.notification = params[:notification] == 'true'
+    @user.save
+    render json: { notification: @user.notification }
   end
 end
